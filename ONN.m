cd1dbbba9ded2a75f65e6ff29925a7b690c7fe30
Yannick PORTO <Yannick PORTO>
2016-10-18 18:30:37 +0200

    Sift descriptor computed

--- ONN.m
+++ ONN.m
@@ -3,3 +3,9 @@ close all;
 clc;
 
 %% Online nearest neighbor
+I = single(rgb2gray(imread('img009.bmp')));
+imshow(I, []);
+
+% Use functions from http://www.vlfeat.org/ toolbox
+[F, D] = vl_sift(I);
+vl_plotsiftdescriptor(D, F);
